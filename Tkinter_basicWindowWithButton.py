81e853dcd9c440ac4c6ed44819f1aef80ba24a2e
Hamza Moumni <94747@supinfo.com>
2015-09-22 11:36:53 -0700

    First drill with Tkinter to show the root window and a button

---
+++ Tkinter_basicWindowWithButton.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+button = ttk.Button(root , text ="Push her")
+button.pack()
+text = Label(root, text = " This is some random text")
+text.pack()
+root.mainloop()
